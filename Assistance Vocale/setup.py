5e205d05707cca59ec6fd6d3edb27f9f4eccf514
Maxime ELIAS <maxime_elias@ens.univ-artois.fr>
2024-10-15 10:54:43 +0200

    maj 2

---
+++ Assistance Vocale/setup.py
@@ -0,0 +1,9 @@
+from cx_Freeze import setup, Executable
+
+# Remplacez 'assistance-voc.py' par le nom de votre script Python
+setup(
+    name="assistance-voc.py",
+    version="1.0",
+    description="Description de votre application",
+    executables=[Executable("assistance-voc.py")]
+)
